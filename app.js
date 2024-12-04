86c96add1b0373b0b69881654c6b2c7bf3a2053b
Madhav Nagpal <madhav@getcerta.com>
2024-12-04 19:35:32 +0530

    hotfix direct to qa

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 function hello() { return 'hello'; }
 function hotfix() { return 'fixed'; }
 function featureToDevelopment() { return 'feature to development'; }
+function hotFixDirectToQA1 () { return 'hotfix direct to QA'; }
